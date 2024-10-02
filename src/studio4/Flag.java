0b7137578688d5ced4d8a95c0a18a3f2ebcde243
davidmaciuca <davidmaciuca@localhost>
2024-10-02 15:19:52 -0500

    done lol

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,11 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(1.0 / 6.0, 0.5, 1.0 / 6.0, 1.0);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledRectangle(3.0 / 6.0, 0.5, 1.0 / 6.0, 1.0);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(5.0 / 6.0, 0.5, 1.0 / 6.0, 1.0);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -9,16 +9,48 @@ import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
-/**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
- */
 public class InterpretDrawingFile {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        // Open file chooser
+        JFileChooser chooser = new JFileChooser("resources");
+        chooser.showOpenDialog(null);
+        File f = new File(chooser.getSelectedFile().getPath());
+        
+        // Create a Scanner to read the file
+        Scanner in = new Scanner(f);
+
+        // Read shape information (e.g., shape type, color, dimensions)
+        String shapeType = in.next(); // e.g., "rectangle", "ellipse"
+        int red = in.nextInt(); // Color values
+        int green = in.nextInt();
+        int blue = in.nextInt();
+        boolean filled = in.nextBoolean(); // Whether the shape is filled
+        double x = in.nextDouble(); // Shape coordinates or size
+        double y = in.nextDouble();
+        double halfWidth = in.nextDouble();
+        double halfHeight = in.nextDouble();
+        
+        // Set the color based on RGB values
+        StdDraw.setPenColor(red, green, blue);
+
+        // Draw the shape based on its type
+        if (shapeType.equals("rectangle")) {
+            if (filled) {
+                StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+            } else {
+                StdDraw.rectangle(x, y, halfWidth, halfHeight);
+            }
+        } else if (shapeType.equals("ellipse")) {
+            if (filled) {
+                StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+            } else {
+                StdDraw.ellipse(x, y, halfWidth, halfHeight);
+            }
+        }
+        // Additional shape types can be added here
+
+        // Close the scanner
+        in.close();
+    }
 }
